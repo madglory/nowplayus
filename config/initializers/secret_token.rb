d1e0f24f6e7ad5a1297cb0508c9288cc287d9248
Mubashar Iqbal <mubs@>
2012-11-23 13:11:46 -0500

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nowplayus::Application.config.secret_token = 'd3e9c25bf75b5c2da3fc0c68ea8adb3943e96726d2643daccb0c3ac37a463971754b9c2eb7fa91efea6cbde3bc89a82aaad326ec98bea7330c1caf643a896207'
