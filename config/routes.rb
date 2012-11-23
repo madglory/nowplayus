8454fb78b8821145341410d42b0ca119d3047354
Mubashar Iqbal <mubs@>
2012-11-23 13:30:46 -0500

    sorcery core

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Nowplayus::Application.routes.draw do
+
+  resources :user_sessions
+  resources :users
+    
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
