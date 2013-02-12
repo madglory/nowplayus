e200328b2c0c655fb51e814afde0750fc8d7c514
Erik Straub <erik@fiveeighteen.net>
2013-02-12 17:35:46 -0500

    Updated models/db, etc.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,37 +13,35 @@ class EventsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    @event = @user.events_created.find(params[:id])
+    @event = @user.events_created.find(params[:id], include: :players)
+    @event_owner = (current_user == @event.user ? 'mine' : 'theirs')
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: [@user, @event] }
+      format.json { render json: [current_user, event] }
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
   def new
     @user = current_user
     @event = @user.events_created.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @user }
+      format.json { render json: @event }
     end
   end
 
   def create
-    @user = current_user
-    @event = @user.events_created.new(params[:event])
+    event = current_user.events_created.new(params[:event])
 
     respond_to do |format|
-      if @event.save
-        format.html { redirect_to user_events_path(@user), notice: 'Event was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+      if event.save
+        format.html { redirect_to user_events_path(current_user), notice: 'Event was successfully created.' }
+        format.json { render json: event, status: :created }
       else
         format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: event.errors, status: :unprocessable_entity }
       end
     end
   end
