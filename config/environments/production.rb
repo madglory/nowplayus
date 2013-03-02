f0276c62f2281b05ac8daf9de7d4209c620f2154
Mubashar Iqbal <mubashariqbal.com@gmail.com>
2013-03-02 10:28:57 -0500

    trying to fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,8 @@ Nowplayus::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
-  config.threadsafe!
+  # vv TODO: SEE IF THIS NEEDS TO BE ENABLED
+  # config.threadsafe!
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
