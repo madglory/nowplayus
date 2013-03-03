90f1819587f6cb892179308394e1e47859ab331d
Mubashar Iqbal <mubashariqbal.com@gmail.com>
2013-03-03 11:41:00 -0500

    fix contest page'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
-  skip_before_filter :require_login, :only => [:index]
+  skip_before_filter :require_login
   layout 'home'
 
   def index
     redirect_to events_url if current_user
   end
+
+  def contest
+    render 'pages/contest', :layout => 'application'
+ end
 end
