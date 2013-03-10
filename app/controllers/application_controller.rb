1efa00998d258d40a12d8cf44683f91d77fd175e
Erik Straub <erik@fiveeighteen.net>
2013-03-09 21:57:47 -0500

    Updated set_time_zone around_filter according to this post http://ilikestuffblog.com/2011/02/03/how-to-set-a-time-zone-for-each-request-in-rails/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < ActionController::Base
 
 private
   def set_time_zone
-    old_time_zone, Time.zone = Time.zone, (logged_in? ? current_user.time_zone : ::Rails.application.config.time_zone)
+    old_time_zone = Time.zone
+    Time.zone = current_user.time_zone if logged_in?
     yield
   ensure
     Time.zone = old_time_zone
