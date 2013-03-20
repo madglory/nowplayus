f80e7c5c8145d4ef1e4dce976fb763e22015e49e
Erik Straub <erik@fiveeighteen.net>
2013-03-20 13:58:51 -0400

    Updated Twitter notification token

--- app/models/twitter_notification_sender.rb
+++ app/models/twitter_notification_sender.rb
@@ -4,9 +4,18 @@ class TwitterNotificationSender
   def initialize(twitter_notification)
     @user = twitter_notification.user
     @message = twitter_notification.message
+    
     raise TwitterNotificationError, 'No Twitter Authentication' unless twitter_authentication
-    @nowplayus = Twitter::Client.new oauth_token: ENV['TWITTER_OAUTH_TOKEN'], oauth_token_secret: ENV['TWITTER_OAUTH_TOKEN_SECRET']
-    @recipient = Twitter::Client.new oauth_token: credentials[:token], oauth_token_secret: credentials[:token_secret]
+    
+    @nowplayus = Twitter::Client.new(
+      oauth_token: ENV['TWITTER_NOTIFICATION_TOKEN'],
+      oauth_token_secret: ENV['TWITTER_NOTIFICATION_TOKEN_SECRET'],
+      consumer_key: ENV['TWITTER_NOTIFICATION_KEY'],
+      consumer_secret: ENV['TWITTER_NOTIFICATION_SECRET'])
+
+    @recipient = Twitter::Client.new(
+      oauth_token: credentials[:token],
+      oauth_token_secret: credentials[:token_secret])
   end
 
   def self.send!(twitter_notification)
