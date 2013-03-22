905e0066a924386e613c28a492acf8fb4bb740c6
Brian Corrigan <brian@agoragames.com>
2013-03-21 20:13:48 -0400

    Renames application.js.coffe, adds UTF8 meta tags

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require jquery-ui
+//= require jquery_nested_form
+//= require select2
+//= require game_selector
+//= require_self
+$(document).foundation();
--- app/assets/javascripts/application.js.coffee
+++
@@ -1,9 +0,0 @@
-#= require jquery
-#= require jquery_ujs
-#= require foundation
-#= require jquery-ui
-#= require jquery_nested_form
-#= require select2
-#= require game_selector
-#= require_self
-$(document).foundation();
