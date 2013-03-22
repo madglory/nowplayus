14c005030947887c1be1f962c17e221e1fa9de5b
Erik Straub <erik@fiveeighteen.net>
2013-03-22 10:42:56 -0400

    Added helper for now<span class="blue">play</span>us

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
         :autolink => true, :space_after_headers => true)
     markdown.render(Sanitize.clean(text)).html_safe
   end
+
+  def site_name
+    content = "now"
+    content << content_tag(:span, 'play', class: 'blue')
+    content << "us"
+    content.html_safe
+  end
+
+  def tooltip(text, tip, options = {})
+    options[:class] = "#{options[:class]} has-tip"
+    options[:data] ||= {}
+    options[:data][:width] ||= '300px'
+    options[:data].merge! tooltip: ''
+    options.merge! title: tip
+    content_tag :span, text, options
+  end
 end
